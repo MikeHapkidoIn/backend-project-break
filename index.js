6c22aeb84d7afbcbc3f83b214155f8fd259d9ef2
Miguel Angel <mangelveragomez@gmail.com>
2025-06-04 10:26:51 +0200

    swagger

--- index.js
+++ index.js
@@ -9,30 +9,35 @@ app.listen(PORT, () => console.log(`http://localhost:${PORT}`));*/
 
 
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 require('dotenv').config();
-const dbConnecction = require('./config/db.js')
+
+const dbConnecction = require('./config/db.js');
 const productRoutes = require('./routes/productRoutes');
 const methodOverride = require('method-override');
 
-app.use(express.urlencoded({ extended: true }))
+// 👉 SWAGGER
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // Mira este log para confirmar que está detectando el método
     console.log('Método override detectado:', req.body._method);
     return req.body._method;
   }
 }));
-app.use(express.json())
+app.use(express.json());
 
+// Rutas
 app.use('/', productRoutes);
 
-
-
-
+// Conexión y servidor
 const PORT = process.env.PORT;
-dbConnecction()
- 
+dbConnecction();
+
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
